eb2a407a53534d6636e0abf79d881b567395982d
Mahmud Nihal <140476865+nihal6251@users.noreply.github.com>
2023-09-21 12:44:15 +0600

    Add files via upload
    Arduino code for soil moisture

---
+++ Soil moisture rgb.ino
@@ -0,0 +1,42 @@
+// C++ code
+//
+int sensor=A0;
+int red=7,blue=6,green=5;
+void setup()
+{
+  Serial.begin(9600);
+  pinMode (red,OUTPUT);
+  pinMode (blue,OUTPUT);
+  pinMode (green,OUTPUT);
+}
+
+void loop()
+{
+  int moisture=analogRead(sensor);
+  Serial.println(moisture);
+  if(moisture>=0 && moisture<=300){
+    Red();
+  }
+  if(moisture>300 && moisture<=600){
+    Yellow();
+  }
+  if(moisture>600){
+    Green();
+  }
+}
+
+void Red(){
+  digitalWrite(red,HIGH);
+  digitalWrite(blue,LOW);
+  digitalWrite(green,LOW);
+}
+void Yellow(){
+  digitalWrite(red,HIGH);
+  digitalWrite(green,HIGH);
+  digitalWrite(blue,LOW);
+}
+void Green(){
+  digitalWrite(green,HIGH);
+  digitalWrite(blue,LOW);
+  digitalWrite(red,LOW);
+}
