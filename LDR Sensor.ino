efefe1b43393881f465989cd2bf84c6c3752ea1b
Mahmud Nihal <140476865+nihal6251@users.noreply.github.com>
2023-09-24 20:16:09 +0600

    Add files via upload

---
+++ LDR Sensor.ino
@@ -0,0 +1,22 @@
+// C++ code
+//LDR Sensor
+int light=9;
+void setup()
+{
+  Serial.begin(9600);
+  pinMode(light,OUTPUT);
+}
+
+void loop()
+{
+ int resistance=analogRead(A0);
+  Serial.println(resistance);
+  if(resistance<400){
+    digitalWrite(light,HIGH);
+    delay(1000);
+  }
+  else{
+    digitalWrite(light,LOW);
+}
+    
+}
